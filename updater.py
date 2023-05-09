518d987351056c3fee6e6acb045432cc922fa50b
Thomas <102198928+0daxelagnia@users.noreply.github.com>
2023-05-09 00:15:26 -0400

    Updated updater.py

--- updater.py
+++ updater.py
@@ -1,1 +1,28 @@
+import requests
+import json
+import os
+import filecmp
 
+
+latest = '3.0.3'
+
+needed_files = [os.path.join('.', 'items.json')]
+source_code_files = [os.path.join('.', 'main.py'), os.path.join('.', '_utils.py_'), os.path.join('.', '_profile_.py'), os.path.join('.', '_options_.py'), os.path.join('.', '_global_.py')]
+
+gh_items: str = 'https://raw.githubusercontent.com/0daxelagnia/SAS4Tool/main/items.json'
+releases_url: str = f'https://github.com/0daxelagnia/SAS4Tool/releases/download/{latest}/'
+
+def check_py_updater_ver():
+    updater_content: str = 'https://github.com/0daxelagnia/SAS4Tool/blob/main/updater.py'
+    open('updater.tmp', 'wb').write(requests.get(updater_content).content)
+    if filecmp.cmp('updater.py', 'updater.tmp'):
+        print('file is the same')
+    else:
+        print('file is not the same')
+
+
+def check_compiled_updater_ver():
+    ...
+
+
+check_py_updater_ver()
