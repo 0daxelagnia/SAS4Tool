904a06f49251c39f04ac2b5a0be2d91af14a2a85
Thomas <102198928+0daxelagnia@users.noreply.github.com>
2023-06-07 23:57:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from _utils_ import menu, option_generatorfrom _global_ import global_menufrom _profile_ import profile_menufrom _options_ import options_menudef main_menu():    option_generator(menu, [global_menu, profile_menu, options_menu])if __name__ == "__main__":    main_menu()
+from _utils_ import menu, option_generator
+from _global_ import global_menu
+from _profile_ import profile_menu
+from _options_ import options_menu
+
+
+def main_menu():
+    option_generator(menu, [global_menu, profile_menu, options_menu])
+
+if __name__ == "__main__":
+    main_menu()
